b211f8da12836e27d7428bd492662c18f66503de
去年夏天 <1823602475@qq.com>
2025-07-29 22:44:47 +0800

    添加打印日志

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-;
 (function () {
-    const box = document.querySelector('div.box');
+	const box = document.querySelector("div.box");
 
-    box.onclick = e => {
-        if (e.target.classList.contains('item')) {
-            console.error(2)
-        }
-    }
-})()
+	box.onclick = (e) => {
+		if (e.target.classList.contains("item")) {
+			console.error(1);
+			console.error(2);
+		}
+	};
+})();
